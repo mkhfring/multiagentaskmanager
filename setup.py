d36c01495332f0838e61e247ff9619fed2385b09
Mohamad <khajezade.mohamad@gmail.com>
2020-09-13 17:41:59 +0430

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import os.path
+import re
+
+
+setup(
+    name='multi-agent-task-manager',
+    description='A multi agent task manager',
+    packages=find_packages(),
+#    entry_points={
+#        'console_scripts': [
+#            'kiosk = src.cli.starter:main'
+#        ]
+#    },
+)
