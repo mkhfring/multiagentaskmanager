fda1013c8f19b7125cc1e67f9095b2212ae58b0b
mkhfring <m.khajezade@carrene.com>
2020-11-07 14:52:46 +0330

    Check for finished tasks

--- multiagent/agents.py
+++ multiagent/agents.py
@@ -5,13 +5,14 @@ import requests
 
 
 class Agent:
-    def __init__(self, manager, scanner_id):
+    def __init__(self, manager, scanner_id, scanner_name):
         self.task_index = 0
         self.manager = manager
         self.scanner_id = scanner_id
+        self.scanner_name = scanner_name
 
     def add_agent_result_to_manager(self):
-        self.manager.task_result[self.scanner_id] = {
+        self.manager.task_result[self.scanner_name] = {
             'current_task':None,
             'finished_tasks': list(),
             'progress':None
@@ -22,62 +23,25 @@ class Agent:
 
         while self.task_index != len(self.manager.tasks):
             task = self.manager.tasks[self.task_index]
-            scan_result = requests.post(
+            self.manager.task_result[self.scanner_name]['current_task'] = task
+            scan_request = requests.post(
             'http://192.168.40.34:8081/api/v1/scanner/file',
             files={'file':open(task, 'rb')},
             data={'scanner_id': self.scanner_id},
             auth=('admin', 'admin')
             )
-            self.manager.task_result[self.scanner_id]['current_task'] = \
-                scan_result.json()['md5']
+            while True:
+                scan_result = requests.get(
+                    'http://192.168.40.34:8081/api/v1/result/md5/{}/'.format(
+                    scan_request.json()['md5']),
+                    auth=('admin', 'admin')
+                )
+                if scan_result.status_code == 200:
+                    break
+                assert 1==1
+            self.manager.task_result[self.scanner_name]['finished_tasks'].append(task)
             self.task_index += 1
 
-#        while self.task_index != len(self.manager.tasks):
-#            task = self.manager.tasks[self.task_index]
-#            self.manager.task_result[self.name]['current_task'] = task
-#
-#            self.manager.task_result[self.name]
-#            print (f"{self.name} {task}")
-#            self.manager.task_result[self.name]['finished_tasks'].append(task)
-#            self.task_index += 1
-#            if self.name == 'Hello':
-#                time.sleep(1)
-#            else:
-#                time.sleep(2)
+        self.manager.number_of_finished_agents += 1
+        assert 1 == 1
 
-
-#class Greeting(Agent):
-#    def __init__(self, manager):
-#        super().__init__(manager)
-#        self.name = 'Greeting'
-#
-#
-#    async def consum_tasks(self):
-#        self.add_agent_result_to_manager()
-#        while self.task_index != len(self.manager.tasks):
-#            task = self.manager.tasks[self.task_index]
-#            self.manager.task_result[self.name]['current_task'] = task
-#
-#            self.manager.task_result[self.name]
-#            print (f"hello {task}")
-#            self.manager.task_result[self.name]['finished_tasks'].append(task)
-#            self.task_index += 1
-#            await asyncio.sleep(1)
-#
-#
-#class Goodby(Agent):
-#    def __init__(self, manager):
-#        super().__init__(manager)
-#        self.name = 'Goodby'
-#
-#    async def consum_tasks(self):
-#        self.add_agent_result_to_manager()
-#        while self.task_index != len(self.manager.tasks):
-#            task = self.manager.tasks[self.task_index]
-#            self.manager.task_result[self.name]['current_task'] = task
-#            print (f"Goodby {task}")
-#            self.manager.task_result[self.name]['finished_tasks'].append(task)
-#            self.task_index += 1
-#            await asyncio.sleep(2)
-#
-#
--- multiagent/multiagent.py
+++ multiagent/multiagent.py
@@ -10,7 +10,7 @@ response = requests.get(
     'http://192.168.40.34:8081/api/v1/scanners',
     auth=('admin', 'admin')
 )
-scanners_id = [item['id'] for item in response.json()]
+scanners = [(item['id'], item['name']) for item in response.json()]
 
 
 
@@ -18,8 +18,11 @@ class MultiAgentTaskManager:
 
     def __init__(self, task_list):
         self.tasks = task_list
-        self.agents = [Agent(self, scanner_id) for scanner_id in scanners_id]
+        self.agents = [
+            Agent(self, scanner[0], scanner[1]) for scanner in scanners
+        ]
         self.task_result = {}
+        self.number_of_finished_agents = 0
 
     def consum(self):
 #        loop = asyncio.get_event_loop()
@@ -30,7 +33,8 @@ class MultiAgentTaskManager:
 #        ]
 #        loop.run_until_complete(asyncio.gather(*tasks))
         for agent in self.agents:
-            t = Thread(target=agent.consum_tasks)
+#            agent.consum_tasks()
+            t = Thread(target=agent.consum_tasks, daemon=True)
             t.start()
 
     @property
@@ -40,4 +44,7 @@ class MultiAgentTaskManager:
     @property
     def get_task_result(self):
         return self.task_result
+
+    def tasks_are_finished(self):
+        return self.number_of_finished_agents == len(self.agents)
 
